fe22ef7bd32b733825f56b73547281a910577d58
Cheng Gong <cheng.gong@mail.mcgill.ca>
2015-03-09 19:13:08 -0400

    working on q6

--- q6update.sql
+++ q6update.sql
@@ -1,4 +1,12 @@
--- 1. Harry posts a picture
+-- 1. Harry posts a picture (inserting the result of a query)
+insert into service (serviceid, creationDate) VALUES (101, CURRENT_TIMESTAMP);
+insert into picture (serviceID,pictureID,memberID,fileSize,url) VALUES (101, 51, (select memberid from member where name='''Harry'''), 200, 'img1.png');
 -- 2. Scott added Jacqueline as a friend
+select friendlistid from friendlist where memberid=(select memberid from member where name='''Jacqueline''');
+select friendlistid from friendlist where memberid=(select memberid from member where name='''Scott''');
+
+
+insert into contains (friendListID, memberID) values ((select friendlistid from friendlist where memberid=(select memberid from member where name='''Jacqueline''')), (select memberid from member where name='''Scott'''));
+
 -- 3. Scott tagged Harry in a picture
 -- 4. Jacqueline sends a message to Harry
